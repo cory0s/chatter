37d85eaf64cac70a402b793108a61ea9ff6c4a8c
Cory Henderson <chenderson19899@gmail.com>
2019-11-20 16:48:29 -0800

    useEffect to limit rerendering

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import { joinRoom, leaveRoom } from '../api/api'
+import io from 'socket.io-client';
+import queryString from 'query-string';
+
+let socket;
 
 const Chat = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:8000';
     
+    useEffect(() => {
+        const { name, room } = queryString.parse(window.location.search);
+
+        socket = io(ENDPOINT);
+        setName(name);
+        setRoom(room);
+        console.log(socket);
+        
+    }, [ENDPOINT, window.location.search]);
+
     return(
-        <h1>Hello</h1>
+        <h1>Hello from CHATTER</h1>
     )
 }
 
