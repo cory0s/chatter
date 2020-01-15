0cc431f4253a8fd8a9cfb22f84cf63f8496144b3
Cory Henderson <chenderson19899@gmail.com>
2020-01-14 22:31:31 -0800

    timestamp

--- src/components/infobar/infobar.js
+++ src/components/infobar/infobar.js
@@ -10,7 +10,7 @@ const InfoBar = ({ room }) => {
             <h3>{room}</h3>
             </div>
             <div className="rightInnerContainer">
-                <a href="/">X</a>
+                <a href="/">Leave Room</a>
             </div>
         </div>
     )
--- src/components/message/message.js
+++ src/components/message/message.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import ReactEmoji from 'react-emoji';
 import './message.css';
 
-const Message = ({ message: { user, text }, name }) => {
+const Message = ({ message: { user, text, date }, name }) => {
     let isSentByCurrentUser = false;
 
     let trimmedName = name.trim().toLowerCase();
@@ -14,14 +14,20 @@ const Message = ({ message: { user, text }, name }) => {
     return(
         isSentByCurrentUser ? 
         (<div className="messageContainer justifyEnd">
-            <p className="sentText pr-10">{trimmedName}</p>
+            <div className="messageInfo">
+                <p className="sentText">{trimmedName}</p>
+                <p className="dateText">{date}</p>
+            </div>
             <div className="messageBox backgroundBlue">
                 <p className="messageText">{text}</p>
             </div>
         </div>)
         : 
         (<div className="messageContainer justifyStart">
-            <p className="sentText pl-10">{user}</p>
+            <div className="messageInfo">
+                <p className="sentText pl-10">{user}</p>
+                <p className="dateText">{date}</p>
+            </div>
             <div className="messageBox backgroundLight">
                 <p className="messageText colorDark">{text}</p>
             </div>
